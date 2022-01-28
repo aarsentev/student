69e039eb98e84e5409d4b801e962ac11b01eb891
arsentev <arsentev@sheridancollege.ca>
2022-01-28 07:32:11 -0500

    reverting the array changes

--- src/student/Student_List.java
+++ src/student/Student_List.java
@@ -10,12 +10,11 @@ package student;
  */
 public class Student_List {
     public static void main(String[] args) {
-        Student_W[] studentList = new Student_W[4];
+        Student_W[] studentList = new Student_W[3];
         
         studentList[0] = new Student_W("s1", "Ronak Sheth");
         studentList[1] = new Student_W("s2", "John Doe");
         studentList[2] = new Student_W("s3", "James Tubbs");
-        studentList[3] = new Student_W("s3", "Sonny Crockett");
         
         for (int i = 0; i < studentList.length; i++){
             System.out.println(studentList[i].getStudentID() + ", " + studentList[i].getStudentName());
